7077e5794c20467f5b9ebe466035d16ab9d1eea7
keksio <keksiaczekuwu@gmail.com>
2024-06-03 23:18:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function printMessage() {
-  document.getElementById('output').innerHTML = 'Happy Wednesday!';
+  document.getElementById('output').innerHTML = 'zajebiste co nie?';
   let someText = document.getElementById('userInput').value;
   document.getElementById('userTextOutput').innerHTML = someText;
 }
