a71e197cd9ae3a6acea5d0d30441b33a78270879
keksio <keksiaczekuwu@gmail.com>
2024-06-03 21:56:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function alertbutton() {
   alert("super tekst")
 }
 function printmessage() {
-document.getElementbyid('output').innerhtml='lepszy tekst'
+document.getElementbyid('output').innerhtml='lepszy tekst';
 }
